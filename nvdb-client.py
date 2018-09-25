8c245d95945606faf7b707a6a7ef93b3e5fa5799
Pawel Buczek <pawel.buczek@ssb.no>
2018-09-25 14:09:31 +0200

    Get road objects

--- nvdb-client.py
+++ nvdb-client.py
@@ -1,4 +1,17 @@
-from json import json
+from pprint import pprint
+
+import requests
+# from json import json
+
 api = "https://www.vegvesen.no/nvdb/api/v2/"
 
-def hent_object:
+
+# https://www.vegvesen.no/nvdb/api/v2/vegobjekter.json
+def get_json(url, parameters, timeout):
+    r = requests.get(url, params=parameters, timeout=timeout)
+    r.raise_for_status()
+    return r.json()
+
+
+road_objects_json = get_json(api + 'vegobjekter.json', {}, 5)
+pprint(road_objects_json)
