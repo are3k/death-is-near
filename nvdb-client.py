180b7fcced68675dc3bea30282c9be31b59fb38f
Stenbrenden, Are <Are.Stenbrenden@ssb.no>
2018-09-25 14:14:56 +0200

    Lint formatting

--- nvdb-client.py
+++ nvdb-client.py
@@ -9,6 +9,7 @@ def get_json(base_url, endpoint):
     url = f"{base_url}{endpoint}.json"
     return get(url).json()
 
+
 if __name__ == '__main__':
     data = get_json(api_base_url, "vegobjekter")
     print(data)
