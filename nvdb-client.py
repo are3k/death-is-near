b131a42567297000ac61d020784b890bbecaedd2
Are Egner-Kaupang <egk@ssb.no>
2018-09-25 14:14:47 +0200

    Simple api client

--- nvdb-client.py
+++ nvdb-client.py
@@ -1,4 +1,15 @@
-from json import json
+import json
+import requests
+from pprint import pprint
+
 api = "https://www.vegvesen.no/nvdb/api/v2/"
 
-def hent_object:
+def hent_objecter(url):
+	response = requests.get(api+url)
+	return response
+
+#url="vegobjekter.json"
+
+url="vegobjekter.json/529Vegreferanse"
+roadObjects = hent_objecter(url)
+pprint(roadObjects.text)
