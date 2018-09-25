0dc69b550a8bc14d768e4cfb4b3db53d81fb1df0
Stenbrenden, Are <Are.Stenbrenden@ssb.no>
2018-09-25 14:11:58 +0200

    Simple beginnings of a API client.

--- nvdb-client.py
+++ nvdb-client.py
@@ -1,4 +1,14 @@
-from json import json
-api = "https://www.vegvesen.no/nvdb/api/v2/"
+from json import load
 
-def hent_object:
+from requests import get
+
+api_base_url = "https://www.vegvesen.no/nvdb/api/v2/"
+
+
+def get_json(base_url, endpoint):
+    url = f"{base_url}{endpoint}.json"
+    return get(url).json()
+
+if __name__ == '__main__':
+    data = get_json(api_base_url, "vegobjekter")
+    print(data)
