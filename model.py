c38062e331c5b23d880c20ce87a1d61597fdda41
linneirin <les@ssb.no>
2018-09-27 17:02:23 +0200

    laget funksjon som kjører alt

--- model.py
+++ model.py
@@ -8,7 +8,6 @@ This is a temporary script file.
 """
 import pandas as pd
 import sklearn
-import os
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
@@ -28,7 +27,8 @@ def split(df, test_size=0.25, rstate=42):
     X = df.iloc[:, :-1].values
     y = df.iloc[:, -1].values
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=test_size, random_state=rstate)
+                                                        test_size=test_size,
+                                                        random_state=rstate)
     return (X_train, X_test, y_train, y_test)
 
 
@@ -68,7 +68,8 @@ def RF_reg(X_train, X_test, y_train, y_test):
 
 def lin_regplot(X, y, model):
     plt.scatter(X[:, 0], y, c='lightblue', label='observasjoner')
-    plt.plot(X[:, 0], model.predict(X), color='red', linewidth=2, label='modell')
+    plt.plot(X[:, 0], model.predict(X), color='red', linewidth=2,
+             label='modell')
     plt.legend()
     #plt.legend()
     plt.xlabel('var1')
@@ -80,37 +81,34 @@ def lin_regplot(X, y, model):
 
 def change_var(df, var, value, model):
     """
-    :param df: data frame
-    :param var: column in df to be changed
-    :param value: value
-    :param model: prediction model
-    :return: new data frame with predictions and differences
-    """
 
+    """
     data = df.copy()
     data[var] += value
+    # old = clf.predict(df)
     new = model.predict(data.iloc[:, :-1])
-    data.iloc[:, -1] = new
-    diff = df.iloc[:, -1] - new
-    data['endring'] = diff
-    data = data.sort_values('endring', ascending=True)
+    data['predikerte ulykker'] = new
+    diff = new - df['ulykker']
+    data['ulykker før endring'] = df['ulykker']
+    data['forskjell'] = diff
+    data = data.sort_values('ulykker før endring', ascending=False)
     return data
+
+def modell(data, kolonne, endring):
+    data_ = pd.read_csv(data, index_col='strekning')
+    X_train, X_test, y_train, y_test = split(data_)
+    trainscore, testscore, pred, clf, imp = RF_reg(X_train, X_test, y_train,
+                                                   y_test)
+    new_data = change_var(data_, kolonne, endring, model=clf)
+    return new_data.iloc[:10,:]
+
 
 
 if __name__ == '__main__':
-    data_532 = pd.read_csv('C:/Users/LES/PycharmProjects/Ny_filtype/532-vegref.csv',
-                       sep=';', index_col='vegreferanse')
-    data_532['var1'] = np.random.randint(2, size=len(data_532.index)).astype('float64')
-    data_532['var2'] = np.random.randint(2, size=len(data_532.index)).astype('float64')
-    data_532 = data_532.iloc[:,-2:]
-    data_532['ulykke'] = np.random.randint(30, size=len(data_532.index)).astype('float64')
-    X_train, X_test, y_train, y_test = split(data_532)
+    res = modell(data='C:/Users/LES/PycharmProjects/XY-matrise.csv',
+                 kolonne='trafikkmengde', endring= -10)
+    print(np.shape(res))
+    print(res)
 
-    trainscore, testscore, pred, clf, imp = RF_reg(X_train, X_test, y_train, y_test)
 
-    lin_regplot(X_train, y_train, clf)
 
-    # Eks
-    X_old = pd.DataFrame({'var1': [0, 1], 'var2': [2, 3], 'ulykke': [10, 16]})
-
-    X_new = change_var(X_old, 'var1', 10, model=clf)
